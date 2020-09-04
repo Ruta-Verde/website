382c5c77534a2f506ae40fe9fa4630fb80890197
will <w.ceriale11@gmail.com>
2020-09-04 00:16:33 -0700

    Create different route paths. Change title to Ruta Verde. Add vue.config.js for general Vue configurations.

---
+++ src/views/Donate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="donate">
+    <h1>This is the donate page</h1>
+  </div>
+</template>
+
+<!--<script>-->
+<!--export default {-->
+<!--name: "Donate"-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
